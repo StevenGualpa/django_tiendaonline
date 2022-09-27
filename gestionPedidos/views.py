d204cee2dd5691a799e2c5bd4c6819e4075aa3c7
Steven Gualpa <stevengualpa@gmail.com>
2022-09-27 15:12:52 -0500

    version 05
    Se agrega Token
    Envia Correos :v

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -30,4 +30,8 @@ def buscarp(request):
 
 def contacto(request):
     
+    if request.method=="POST":
+        
+        return render(request,"gracias.html")
+
     return render(request,"contacto.html")
